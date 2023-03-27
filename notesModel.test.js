cd3247cae225146ce18861fc0d3c9cfb393f0ab4
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-03-27 15:17:47 +0100

    Added NotesModel class and tests.

---
+++ notesModel.test.js
@@ -0,0 +1,28 @@
+const NotesModel = require ('./notesModel');
+
+describe('NotesModel.', () => {
+
+    let notes = new NotesModel();
+
+    beforeEach( () => {
+        const notes = new NotesModel(); 
+    });
+
+    it('Initializes with an empty notes list.', () => {
+        expect(notes).toBeInstanceOf(NotesModel);
+        expect(notes.getNotes()).toEqual ([]);
+    });
+
+    it('Adds a note to the list.', () => {
+        notes.addNote('Buy milk');
+        notes.addNote('Go to the gym');
+        expect(notes.getNotes()).toEqual (['Buy milk','Go to the gym'])
+    });
+
+    it('Resets the list of notes.', () => {
+        notes.addNote('Buy milk');
+        notes.addNote('Go to the gym');
+        notes.reset();
+        expect(notes.getNotes()).toEqual ([])
+    });
+});
