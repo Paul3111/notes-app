39467082142df6a024bf08c6c264889857d74d06
Paul <lazar_paul_2006@yahoo.com>
2023-06-02 12:35:36 +0100

    Amended index.js.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const NotesModel = require ('./notesModel');
-const NotesView = require ('./notesView');
+// const NotesView = require ('./notesView');
+const NotesViewBrowser = require ('./notesViewBrowser');
 const NotesClient = require ('./notesClient');
 
 const notes = new NotesModel();
 const client = new NotesClient();
-const views = new NotesView(notes, client);
+const views = new NotesViewBrowser(notes, client);
 
 console.log('The notes app is running.');
 
