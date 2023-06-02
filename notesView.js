b7532cb1650db1303e1e80e0d1f227f907dd6f9a
Paul <lazar_paul_2006@yahoo.com>
2023-06-02 12:29:00 +0100

    Amended NotesView.

--- notesView.js
+++ notesView.js
@@ -1,5 +1,5 @@
 class NotesView {
-    constructor(notes, client) {
+/*    constructor(notes, client) {
         this.notes = notes;
         this.client = client;
         this.mainContainerEl = document.querySelector('#main-container');
@@ -20,7 +20,34 @@ class NotesView {
             location.reload();
          });
     }
+*/
+    constructor(notes, client) {
+      this.notes = notes;
+      this.client = client;
+      this.mainContainerEl = null;
+      this.button_1 = null;
+      this.button_2 = null;
+      this.shortcodeRegex = /:[a-zA-Z0-9_]+:/g;
+   }
 
+  initialize() {
+      this.mainContainerEl = document.querySelector('#main-container');
+      this.button_1 = document.querySelector('#button-1');
+      this.button_2 = document.querySelector('#button-2');
+
+      this.button_1.addEventListener('click', () => {
+          const messageInput = document.querySelector('#message-input');
+          const newNote = messageInput.value;
+          this.addNewNote(newNote);
+      });
+
+      this.button_2.addEventListener('click', () => {
+          this.client.reset();
+          this.displayNotesFromApi();
+          location.reload();
+      });
+    }
+    
     async addNewNote(newNote) {
         const emojifiedNote = await this.emojify(newNote);
         this.notes.addNote(emojifiedNote);
